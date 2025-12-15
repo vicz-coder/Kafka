f64e6c7cfa09b40b1cb9410410952b2181751e20
vicz-coder <102509509+vicz-coder@users.noreply.github.com>
2025-12-15 11:05:12 +0530

    Initial Commit

---
+++ src/test/java/com/learnkafka/library_event_producer/LibraryEventProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnkafka.library_event_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryEventProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
